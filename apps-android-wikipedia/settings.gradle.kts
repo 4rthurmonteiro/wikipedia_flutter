00a0c53a81637556bd3d287585db6f264bbaa76e
Arthur Monteiro <monteiroamelo@gmail.com>
2024-12-23 19:44:41 -0300

    chore: updating settings.gradle

--- apps-android-wikipedia/settings.gradle.kts
+++ apps-android-wikipedia/settings.gradle.kts
@@ -15,3 +15,11 @@ dependencyResolutionManagement {
 }
 
 include(":app")
+
+// Replace "flutter_module" with whatever package_name you supplied when you ran:
+// `$ flutter create -t module [package_name]
+val filePath = settingsDir.parentFile.toString() + "/wikipedia_flutter_module/.android/include_flutter.groovy"
+apply(from = File(filePath)
+
+// Note
+// The ability to invoke include_flutter.groovy from Kotlin code requires Flutter 3.27.
